ea530dd4c511c2111a0c0b693592d1d0adf8288e
Jaimie van Santen <jaimie@jaimievansanten.nl>
2014-07-06 23:56:32 +0200

    Show no blogposts + hide loginbutton

--- client/views/pages/blog/blog.js
+++ client/views/pages/blog/blog.js
@@ -1,5 +1,9 @@
 Template.blog.helpers({
     posts: function () {
         return Posts.find({ published: true });
+    },
+
+    hasPosts: function() {
+        return Posts.find().count() > 0;
     }
 });
