bf2050eb62790d04ea44046b7b5e310cb17bd7c2
Jaimie van Santen <jaimie@jaimievansanten.nl>
2014-07-17 23:03:41 +0200

    Only load Kadira in production

--- server/kadira.js
+++ server/kadira.js
@@ -1,2 +1,5 @@
-Kadira.connect(process.env.KADIRA_API_KEY, process.env.KADIRA_API_SECRET)
-
+Meteor.startup(function() {
+  if(process.env.NODE_ENV !== "development") {
+    Kadira.connect(process.env.KADIRA_API_KEY, process.env.KADIRA_API_SECRET);
+  }
+});
