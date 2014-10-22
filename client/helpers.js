4147c2658e0ef3aaad08c685ce9db0d4090eca07
Jaimie van Santen <jaimie@jaimievansanten.nl>
2014-10-22 20:02:02 +0200

    Update helpers to new syntax
    Signed-off-by: Jaimie van Santen <jaimie@jaimievansanten.nl>

--- client/helpers.js
+++ client/helpers.js
@@ -1,17 +1,25 @@
-Handlebars.registerHelper('randomColor', function(context, options) {
+UI.registerHelper('randomColor', function(context, options) {
     colors = ['default', 'primary', 'danger', 'warning', 'info', 'success', 'info'];
 
     return colors[Math.floor(Math.random() * colors.length)];
 });
 
-Handlebars.registerHelper('formatDateFromNow', function(context, options) {
+UI.registerHelper('formatDateFromNow', function(context, options) {
     return moment(context).calendar();
 });
 
-Handlebars.registerHelper('ellipsis', function(context, options) {
+UI.registerHelper('ellipsis', function(context, options) {
     return context.slice(0, 360).replace(/.{3}$/g, '…');
 });
 
-Handlebars.registerHelper('getYear', function(context, options) {
+UI.registerHelper('getYear', function(context, options) {
     return new Date(context).getFullYear();
+});
+
+UI.registerHelper('typed', function(context, options) {
+    return 
+    $.this("<p class='typed-paragraph' />").typed({
+      strings: context,
+      typeSpeed: 0
+    });
 });
